038453a1013c6c663fac8fc989864a7a03898ce1
illian01 <wnsgh578@naver.com>
2019-10-06 16:22:35 +0900

    Rewriting dlg (#1)
    * Reconstruct dialog
    
    * Remove all action in dialog
    
    * Remove unuse field in dialog

--- src/stopwait/ARPDlg.java
+++ src/stopwait/ARPDlg.java
@@ -41,26 +41,22 @@ public class ARPDlg extends JFrame implements BaseLayer {
 
 	private static LayerManager m_LayerMgr = new LayerManager();
 
-	private JTextField ChattingWrite;
+	private JTextField ARPCacheInputField;
+	private JTextField GratuitousARPInputField;
 
 	Container contentPane;
 
-	JTextArea ChattingArea;
-	JTextArea srcMACAddress;
-	JTextArea dstMACAddress;
-
-	JLabel lblNIC;
-	JLabel lblsrc;
-	JLabel lbldst;
-
-	JButton Setting_Button;
-	JButton Chat_send_Button;
-
-	static JComboBox<String> NICComboBox;
-
-	int adapterNumber = 0;
-
-	String Text;
+	JTextArea ARPCacheTextArea;
+	JTextArea ProxyARPTextArea;
+	
+	JButton ARPCacheItemDeleteButton;
+	JButton ARPCacheAllDeleteButton;
+	JButton ARPCacheSendButton;
+	JButton ProxyARPAddButton;
+	JButton ProxyARPDeleteButton;
+	JButton GratuitousARPSendButton;
+	JButton TerminateButton;
+	JButton CancelButton;
 
 	public static void main(String[] args) throws SocketException {
 		// TODO Auto-generated method stub
@@ -73,179 +69,126 @@ public class ARPDlg extends JFrame implements BaseLayer {
 
 	public ARPDlg(String pName) throws SocketException {
 		pLayerName = pName;
-
-		setTitle("SWP");
+		
+		// TestARP window 
+		setTitle("TestARP");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(250, 250, 644, 425);
+		setBounds(250, 250, 837, 450);
 		contentPane = new JPanel();
 		((JComponent) contentPane).setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JPanel chattingPanel = new JPanel();// chatting panel
-		chattingPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "chatting",
+		// ARP Cache part
+		JPanel ARPCachePanel = new JPanel();
+		ARPCachePanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "ARP Cache",
 				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		chattingPanel.setBounds(10, 5, 360, 276);
-		contentPane.add(chattingPanel);
-		chattingPanel.setLayout(null);
+		ARPCachePanel.setBounds(10, 5, 400, 340);
+		contentPane.add(ARPCachePanel);
+		ARPCachePanel.setLayout(null);
 
-		JPanel chattingEditorPanel = new JPanel();// chatting write panel
-		chattingEditorPanel.setBounds(10, 15, 340, 210);
-		chattingPanel.add(chattingEditorPanel);
-		chattingEditorPanel.setLayout(null);
+		ARPCacheTextArea = new JTextArea();
+		ARPCacheTextArea.setEditable(false);
+		ARPCacheTextArea.setBounds(10, 20, 380, 210);
+		ARPCachePanel.add(ARPCacheTextArea);
 
-		ChattingArea = new JTextArea();
-		ChattingArea.setEditable(false);
-		ChattingArea.setBounds(0, 0, 340, 210);
-		chattingEditorPanel.add(ChattingArea);// chatting edit
-
-		JPanel chattingInputPanel = new JPanel();// chatting write panel
-		chattingInputPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		chattingInputPanel.setBounds(10, 230, 250, 20);
-		chattingPanel.add(chattingInputPanel);
-		chattingInputPanel.setLayout(null);
-
-		ChattingWrite = new JTextField();
-		ChattingWrite.setBounds(2, 2, 250, 20);// 249
-		chattingInputPanel.add(ChattingWrite);
-		ChattingWrite.setColumns(10);// writing area
-
-		JPanel settingPanel = new JPanel();
-		settingPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "setting",
+		ARPCacheItemDeleteButton = new JButton("Item Delete");
+		ARPCacheItemDeleteButton.setBounds(40, 240, 150, 40);
+		ARPCacheItemDeleteButton.addActionListener(new setAddressListener());
+		ARPCachePanel.add(ARPCacheItemDeleteButton);// chatting send button
+		
+		ARPCacheAllDeleteButton = new JButton("All Delete");
+		ARPCacheAllDeleteButton.setBounds(210, 240, 150, 40);
+		ARPCacheAllDeleteButton.addActionListener(new setAddressListener());
+		ARPCachePanel.add(ARPCacheAllDeleteButton);
+		
+		JLabel ARPCacheIPAddressLabel = new JLabel("IP주소");
+		ARPCacheIPAddressLabel.setBounds(10, 290, 40, 30);
+		ARPCachePanel.add(ARPCacheIPAddressLabel);
+		
+		ARPCacheInputField = new JTextField();
+		ARPCacheInputField.setBounds(55, 290, 260, 30);
+		ARPCachePanel.add(ARPCacheInputField);
+		
+		ARPCacheSendButton = new JButton("Send");
+		ARPCacheSendButton.setBounds(320, 290, 70, 30);
+		ARPCacheSendButton.addActionListener(new setAddressListener());
+		ARPCachePanel.add(ARPCacheSendButton);
+		
+		// Proxy ARP Entry part
+		JPanel ProxyARPEntryPanel = new JPanel();
+		ProxyARPEntryPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Proxy ARP Entry",
 				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		settingPanel.setBounds(380, 5, 236, 371);
-		contentPane.add(settingPanel);
-		settingPanel.setLayout(null);
+		ProxyARPEntryPanel.setBounds(415, 5, 400, 250);
+		contentPane.add(ProxyARPEntryPanel);
+		ProxyARPEntryPanel.setLayout(null);
 
-		JPanel NICPanel = new JPanel();
-		NICPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		NICPanel.setBounds(10, 46, 170, 20);
-		settingPanel.add(NICPanel);
-		NICPanel.setLayout(null);
+		ProxyARPTextArea = new JTextArea();
+		ProxyARPTextArea.setEditable(false);
+		ProxyARPTextArea.setBounds(10, 20, 380, 170);
+		ProxyARPEntryPanel.add(ProxyARPTextArea);// src address
 
-		lblNIC = new JLabel("NIC선택");
-		lblNIC.setBounds(10, 25, 170, 20);
-		settingPanel.add(lblNIC);
-
-		NICComboBox = new JComboBox<>();
-		List<PcapIf> l = ((NILayer) m_LayerMgr.GetLayer("NI")).m_pAdapterList;
-		for (int i = 0; i < l.size(); i++)
-			NICComboBox.addItem(l.get(i).getDescription() + " : " + l.get(i).getName());
-
-		NICComboBox.setBounds(2, 2, 550, 20);
-		NICComboBox.addActionListener(new setAddressListener());
-		NICPanel.add(NICComboBox);// src address
-
-		JPanel sourceAddressPanel = new JPanel();
-		sourceAddressPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		sourceAddressPanel.setBounds(10, 96, 170, 20);
-		settingPanel.add(sourceAddressPanel);
-		sourceAddressPanel.setLayout(null);
-
-		lblsrc = new JLabel("Source Mac Address");
-		lblsrc.setBounds(10, 75, 170, 20);
-		settingPanel.add(lblsrc);
-
-		srcMACAddress = new JTextArea();
-		srcMACAddress.setBounds(2, 2, 170, 20);
-		sourceAddressPanel.add(srcMACAddress);// src address
-
-		JPanel destinationAddressPanel = new JPanel();
-		destinationAddressPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		destinationAddressPanel.setBounds(10, 212, 170, 20);
-		settingPanel.add(destinationAddressPanel);
-		destinationAddressPanel.setLayout(null);
-
-		lbldst = new JLabel("Destination Mac Address");
-		lbldst.setBounds(10, 187, 190, 20);
-		settingPanel.add(lbldst);
-
-		dstMACAddress = new JTextArea();
-		dstMACAddress.setBounds(2, 2, 170, 20);
-		destinationAddressPanel.add(dstMACAddress);// dst address
-
-		Setting_Button = new JButton("Setting");// setting
-		Setting_Button.setBounds(80, 270, 100, 20);
-		Setting_Button.addActionListener(new setAddressListener());
-		settingPanel.add(Setting_Button);// setting
-
-		Chat_send_Button = new JButton("Send");
-		Chat_send_Button.setBounds(270, 230, 80, 20);
-		Chat_send_Button.addActionListener(new setAddressListener());
-		chattingPanel.add(Chat_send_Button);// chatting send button
-
+		ProxyARPAddButton = new JButton("Add");
+		ProxyARPAddButton.setBounds(40, 200, 150, 40);
+		ProxyARPAddButton.addActionListener(new setAddressListener());
+		ProxyARPEntryPanel.add(ProxyARPAddButton);// chatting send button
+		
+		ProxyARPDeleteButton = new JButton("Delete");
+		ProxyARPDeleteButton.setBounds(210, 200, 150, 40);
+		ProxyARPDeleteButton.addActionListener(new setAddressListener());
+		ProxyARPEntryPanel.add(ProxyARPDeleteButton);
+		
+		// Gratuitous ARP part
+		JPanel GratuitousARPPanel = new JPanel();
+		GratuitousARPPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Gratuitous ARP",
+				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		GratuitousARPPanel.setBounds(415, 270, 400, 75);
+		contentPane.add(GratuitousARPPanel);
+		GratuitousARPPanel.setLayout(null);
+		
+		JLabel GratuitousARPHWAddressLabel = new JLabel("HW주소");
+		GratuitousARPHWAddressLabel.setBounds(10, 25, 50, 30);
+		GratuitousARPPanel.add(GratuitousARPHWAddressLabel);
+		
+		GratuitousARPInputField = new JTextField();
+		GratuitousARPInputField.setBounds(60, 25, 255, 30);
+		GratuitousARPPanel.add(GratuitousARPInputField);
+		
+		GratuitousARPSendButton = new JButton("Send");
+		GratuitousARPSendButton.setBounds(320, 25, 70, 30);
+		GratuitousARPSendButton.addActionListener(new setAddressListener());
+		GratuitousARPPanel.add(GratuitousARPSendButton);
+		
+		// Two button on bottom side
+		TerminateButton = new JButton("종료");
+		TerminateButton.setBounds(305, 355, 100, 30);
+		TerminateButton.addActionListener(new setAddressListener());
+		contentPane.add(TerminateButton);
+		
+		CancelButton = new JButton("취소");
+		CancelButton.setBounds(420, 355, 100, 30);
+		CancelButton.addActionListener(new setAddressListener());
+		contentPane.add(CancelButton);
+		
 		setVisible(true);
-
 	}
 
 	class setAddressListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if (e.getSource() == NICComboBox) {
-				srcMACAddress.setText("");
-				NILayer NI = (NILayer) m_LayerMgr.GetLayer("NI");
-				List<PcapIf> l = NI.m_pAdapterList;
-				try {
-					byte[] address = l.get(NICComboBox.getSelectedIndex()).getHardwareAddress();
-					int j = 0;
-					for (byte inetAddress : address) {
-						srcMACAddress.append(String.format("%02x", inetAddress));
-						if (j++ != address.length - 1)
-							srcMACAddress.append("-");
-					}
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-			} else if (e.getSource() == Setting_Button) {
-				if (Setting_Button.getText().equals("Setting")) {
-					
-					if (dstMACAddress.getText().equals("") || srcMACAddress.getText().equals("")) {
-						JOptionPane.showMessageDialog(null, "입력이 없습니다!");
-					}
-					else {
-						NILayer NI = (NILayer) m_LayerMgr.GetLayer("NI");
-						EthernetLayer Eth = (EthernetLayer) m_LayerMgr.GetLayer("Eth");
-
-						Eth.Setenet_dstaddr(dstMACAddress.getText());
-						Eth.Setenet_srcaddr(srcMACAddress.getText());
-						int i = NICComboBox.getSelectedIndex();
-						NI.SetAdapterNumber(i);
-
-						dstMACAddress.setEnabled(false);
-						srcMACAddress.setEnabled(false);
-						NICComboBox.setEnabled(false);
-						Setting_Button.setText("Reset");
-					}
-				} else {
-					dstMACAddress.setEnabled(true);
-					srcMACAddress.setEnabled(true);
-					NICComboBox.setEnabled(true);
-					dstMACAddress.setText("");
-					Setting_Button.setText("Setting");
-				}
-			} else if (e.getSource() == Chat_send_Button) {
-				if (Setting_Button.getText().equals("Setting")) {
-					JOptionPane.showMessageDialog(null, "주소 설정을 먼저 하십시오.");					
-				}
-				else {
-					byte[] input = ChattingWrite.getText().getBytes();
-					ChattingArea.append("[SEND]:" + ChattingWrite.getText() + "\n");
-					p_UnderLayer.Send(input, input.length);
-					ChattingWrite.setText("");
-				}
-			}
+			// 액션 추가 해야함
 		}
 	}
 
 	public boolean Receive(byte[] input) {
-		ChattingArea.append("[RECV]:");
+		ARPCacheTextArea.append("[RECV]:");
 		try {
-			ChattingArea.append(new String(input, "MS949"));
+			ARPCacheTextArea.append(new String(input, "MS949"));
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		ChattingArea.append("\n");
+		ARPCacheTextArea.append("\n");
 
 		return true;
 	}
